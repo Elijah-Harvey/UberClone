5d2440437aedad93e2385d4fd1faa258de4fde39
Elijah-Harvey <potenialtrader@gmail.com>
2022-05-10 22:26:46 -0400

    Updated App.js

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets build uber</Text>
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Lets build uber</Text>
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+
